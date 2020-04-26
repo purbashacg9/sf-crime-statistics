25e724cccd5dfdf422dba6ab3e5b34b18fc4f3b2
Purbasha <purbasha@paradata.io>
2020-04-26 13:38:52 -0700

    remove commented code and TODO tags

--- kafka_server.py
+++ kafka_server.py
@@ -1,17 +1,15 @@
 import producer_server
 
 def run_kafka_server():
-	# TODO get the json file path
+	# get the json file path
     input_file = "police-department-calls-for-service.json"
 
-    # TODO fill in blanks
     producer = producer_server.ProducerServer(
         input_file=input_file,
         topic="org.udacity.assignment.crime-statistics", 
         bootstrap_servers="localhost:9092",
-        client_id="2020"
+        client_id="kafka-server-plain"
     )
-
     return producer
 
 
